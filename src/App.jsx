d35eb7fee7f6e0c91f93aabf4d90a99c95eda1f6
Alif Dwi Rahman <alifdwirahman54@gmail.com>
2025-02-15 16:00:24 +0700

    First commmit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex min-h-screen justify-center items-center">
+      <h2 className="text-3xl font-semibold">Zeitplan</h2>
+    </main>
+  );
+}
+
+export default App;
